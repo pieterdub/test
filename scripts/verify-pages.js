7837e03bcb3bc0607612b59211f3d90145fa27e1
Pieter du Buisson <pieter@chainapi.com>
2023-12-19 16:22:23 +0200

    use script args

--- scripts/verify-pages.js
+++ scripts/verify-pages.js
@@ -1,19 +1,21 @@
-const github = require('@actions/github')
+const { Octokit } = require('octokit')
 
 async function main() {
   const token = process.argv[2];
+  const owner = process.argv[3];
+  const repo = process.argv[4];
 
   process.argv.forEach(function (val, index, array) {
     console.log(index + ': ' + val);
   });
 
-  const octokit = new github.getOctokit({
+  const octokit = new Octokit({
     auth: token
   })
   
-  const result = await octokit.request('GET /repos/{owner}/{repo}/pages/builds/latest', {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
+  const result = await octokit.request('GET /repos/pieterdub/test/pages/builds/latest', {
+    owner,
+    repo,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
