21ea48f593f079453bd561c3fa0b76c32b82cc08
Pieter du Buisson <pieter@chainapi.com>
2023-12-19 15:29:25 +0200

    update script

--- scripts/verify-pages.js
+++ scripts/verify-pages.js
@@ -2,7 +2,12 @@
 
 
 function main() {
-  const token = process.argv[0];
+  const token = process.argv[2];
+
+  process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+  });
+  
   // Verify merkle tree data (before HashRegistry checks)
   console.log("Verifying pages", token)
 }
