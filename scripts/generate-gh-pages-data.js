dc8a25e935fc50b6437efe806b72c43b69d00bdf
Pieter du Buisson <pieter@chainapi.com>
2024-06-10 09:43:28 +0200

    test console log info

--- scripts/generate-gh-pages-data.js
+++ scripts/generate-gh-pages-data.js
@@ -1,73 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
-const { exec } = require('child_process');
-const { ethers } = require('ethers');
-const { createDapiPricingMerkleTree } = require('./utils');
-
-const dapiPricingCurrentHashData = require('../data/dapi-pricing-merkle-tree-root/current-hash.json');
-
-
 async function generateGHPagesData() {
-  await generatePricingRootData();
-  await splitPricingValues();
-
-  const execute = promisify(exec);
-  await execute('yarn format');
+  logSuccessMessage('Successfully validated')
+  logInfoMessage('Skipping validation')
+  logVerificationFailure('Failed validation')
 }
 
-async function generatePricingRootData() {
-  const outputPath = path.join(__dirname, '..', 'market');
-
-  const pricingDataPath = path.join(outputPath, 'dapi-pricing-merkle');
-  fs.mkdirSync(pricingDataPath, { recursive: true });
-
-  const data = {
-    timestamp: dapiPricingCurrentHashData.timestamp,
-    hash: dapiPricingCurrentHashData.hash,
-  };
-
-  fs.writeFileSync(`${pricingDataPath}/data.json`, JSON.stringify(data, null, 4));
+export function logSuccessMessage(message) {
+  console.log('\x1b[32m%s\x1b[0m', `\u2713 ${message}\n`);
 }
 
-async function splitPricingValues() {
-  const outputPath = path.join(__dirname, '..', 'market', 'dapi-pricing-merkle');
+export function logInfoMessage(message) {
+  console.log('\x1b[33m%s\x1b[0m', `\u2298 ${message}\n`);
+}
 
-  const { merkleTreeValues } = dapiPricingCurrentHashData;
-
-  const tree = createDapiPricingMerkleTree(merkleTreeValues);
-  const valuesByChainAndDapiName = {};
-
-  for (const [idx, item] of tree.entries()) {
-    const [hashedDapiName, chainId] = item;
-    const dapiName = ethers.utils.parseBytes32String(hashedDapiName);
-    const name = dapiName.replace('/', '-');
-
-    const proof = tree.getProof(idx);
-    const leaf = { value: item, proof };
-
-    if (!valuesByChainAndDapiName[chainId]) {
-      valuesByChainAndDapiName[chainId] = {};
-    }
-
-    if (!valuesByChainAndDapiName[chainId][name]) {
-      valuesByChainAndDapiName[chainId][name] = [leaf];
-    } else {
-      valuesByChainAndDapiName[chainId][name].push(leaf);
-    }
-  }
-
-  for (const chainId in valuesByChainAndDapiName) {
-    for (const dapiName in valuesByChainAndDapiName[chainId]) {
-      const valueCollection = valuesByChainAndDapiName[chainId][dapiName];
-      const content = { merkleRoot: tree.root, leaves: valueCollection };
-
-      const chainPath = path.join(outputPath, chainId);
-      const filePath = path.join(chainPath, `${dapiName}.json`);
-      fs.mkdirSync(chainPath, { recursive: true });
-      fs.writeFileSync(filePath, JSON.stringify(content, null, 4));
-    }
-  }
+export function logVerificationFailure(err) {
+  const message = err instanceof Error ? err.message : err;
+  console.error('\x1b[31m%s\x1b[0m', `\u2718 Verification failed. ${message}\n`);
 }
 
 generateGHPagesData().catch((error) => {
